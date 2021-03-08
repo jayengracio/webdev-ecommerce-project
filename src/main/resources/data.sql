7eb1d6f2c33f6a584fe90c280ee66609d8638059
John Engracio <johnengracio98@gmail.com>
2021-03-08 16:43:14 +0000

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Product(type, name, details, price) VALUES('food', 'Apple', 'A red apple', 10.00);
+INSERT INTO Product(type, name, details, price) VALUES('electronics', 'Laptop', 'A white laptop', 500.00);
+INSERT INTO Product(type, name, details, price) VALUES('food', 'Banana', 'A banana', 3.00);
+INSERT INTO Product(type, name, details, price) VALUES('food', 'Grapes', 'Some grapes', 3.00);
+INSERT INTO Product(type, name, details, price) VALUES('electronics', 'Computer', 'A computer', 900.00);
+INSERT INTO Product(type, name, details, price) VALUES('book', 'Harry Potter', 'A Harry Potter Book', 40.00);
+INSERT INTO Product(type, name, details, price) VALUES('food', 'Donut', 'A donut', 9.00);
+INSERT INTO Product(type, name, details, price) VALUES('food', 'Pizza', 'A scuffed pizza', 14.00);
+INSERT INTO Product(type, name, details, price) VALUES('electronic', 'Keyboard', 'A broken keyboard', 55.00);
