940c34c5ec6427e13a0c2d6a4baa317cbec0e494
John Engracio <johnengracio98@gmail.com>
2021-03-11 15:43:29 +0000

    Implementation of login and register functionalities
    -> Users can now log in to their account if found in the user database.
    
    -> Can now register a new user into the user database.

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -1,5 +1,10 @@
 package app;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -14,9 +20,19 @@ public class MainController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private User loggedUser = new User();
+    private boolean loggedIn;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        if (!loggedIn)
         return "index.html";
+
+        model.addAttribute("user", loggedUser);
+        return "home.html";
     }
 
     @GetMapping("/login")
@@ -24,6 +40,63 @@ public class MainController {
         return "login.html";
     }
 
+    @PostMapping("/login")
+    public String loggedIn(HttpServletResponse response, @RequestParam String username, @RequestParam String password, Model model) {
+        List<User> users = userRepository.findAll();
+        
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                loggedUser.setPassword(user.getPassword());
+                loggedUser.setUsername(user.getUsername());
+                loggedUser.setId(user.getId());
+
+                loggedIn = true;
+                try {
+                    response.sendRedirect("/");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return "loginfailure.html";
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletResponse response) {
+        loggedUser = new User();
+        loggedIn = false;
+        try {
+            response.sendRedirect("/login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile.html";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register.html";
+    }
+
+    @PostMapping("/register")
+    public void registerUser(HttpServletResponse response, @RequestParam String username, @RequestParam String password) {
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setPassword(password);
+        userRepository.save(newUser);
+
+        try {
+            response.sendRedirect("/");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     @GetMapping("/products")
     public String products(Model model) {
         model.addAttribute("products", productRepository.findAll());
@@ -39,11 +112,6 @@ public class MainController {
     @GetMapping("/cart")
     public String cart() {
         return "cart.html";
-    }
-
-    @GetMapping("/register")
-    public String register() {
-        return "register.html";
     }
 
     @GetMapping("/details/{id}")
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,7 @@ INSERT INTO Product(type, name, details, price) VALUES('book', 'Harry Potter', '
 INSERT INTO Product(type, name, details, price) VALUES('food', 'Donut', 'A donut', 9.00);
 INSERT INTO Product(type, name, details, price) VALUES('food', 'Pizza', 'A scuffed pizza', 14.00);
 INSERT INTO Product(type, name, details, price) VALUES('electronic', 'Keyboard', 'A broken keyboard', 55.00);
+
+INSERT INTO User(username, password, owner) VALUES ('owner', 'password', true);
+INSERT INTO User(username, password, owner) VALUES ('Phil', '1234pass', false);
+INSERT INTO User(username, password, owner) VALUES ('Joseph', 'w0w', false);
